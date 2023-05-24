f734900ea3597141d48d17eabcb69de83c0b1c96
Bilguun <4bilguun@gmail.com>
2023-05-24 21:35:49 +0800

    Added function that rolls dice

---
+++ app.js
@@ -0,0 +1,25 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, player1=0, player2=1
+var activePlayer = 1;
+
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+// Шооны аль талаараа буусныг хадгалах хувьсагч
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "img/dice-" + diceNumber + ".png";
+});
