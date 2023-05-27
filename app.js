f67f013bc2935e9085e41c4a1751dd43394d8834
Bilguun <4bilguun@gmail.com>
2023-05-27 16:15:33 +0800

    Changes winning point 10 to 100

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     document.getElementById("score-" + activePlayer).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
       document
         .querySelector(".player-" + activePlayer + "-panel")
